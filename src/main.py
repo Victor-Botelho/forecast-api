c2bb3e0fc27229792358acb2c941dfa41ebc7cb6
Victor-Botelho <victorbotelho@hotmail.com>
2024-01-15 18:52:37 -0300

    Remove logging functions to make code cleaner

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import time
-import logging
 
-from flask import Flask, request, Response, g
+from flask import Flask, request, g
 import numpy as np
 
 from src.forecast import forecast, cross_validation, get_model
@@ -9,49 +8,10 @@ from src.forecast import forecast, cross_validation, get_model
 
 app = Flask(__name__)
 
-# Setup logging
-logging.basicConfig(filename="./logs/access.log", level=logging.INFO)
-
 @app.route('/', methods=['GET'])
 def index():
     return 'Hello, World!'
 
-
-@app.before_request
-def before_request():
-    g.start_time = time.time()
-
-
-@app.after_request
-def after_request(response):
-    if hasattr(g, 'start_time'):
-        end_time = time.time()
-        elapsed_time = end_time - g.start_time
-
-        timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
-        method = request.method
-        endpoint = request.endpoint
-
-        log_message = (
-            f"Timestamp: {timestamp} - Method: {method} - Endpoint: {endpoint} - "
-            f"Duration (s) {elapsed_time:.6f}"
-        )
-
-        if method == 'POST' and endpoint == 'forecast_':
-            try:
-                data_length = len(request.json.get('series', []))
-                horizon = request.json.get('horizon', 'Not provided')
-                request_model = request.json.get('model', 'Not provided')
-                response_model = response.json.get('model', 'Not provided')
-                log_message += (
-                    f" - Data Length: {data_length} - Horizon: {horizon} - Model: {request_model} -> {response_model}"
-                )
-            except Exception as e:
-                log_message += f" - Error parsing JSON: {str(e)}"
-
-        app.logger.info(log_message)
-
-    return response
 
 @app.route('/forecast', methods=['POST'])
 def forecast_():
